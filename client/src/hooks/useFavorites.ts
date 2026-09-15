5fefb8448e9a578222f4e3ba505d4cb104d71f44
Yonatan Eiferman <yonieifer@gmail.com>
2026-09-15 11:50:14 +0300

    add useFavorites hooks

--- client/src/hooks/useFavorites.ts
+++ client/src/hooks/useFavorites.ts
@@ -1,9 +1,55 @@
-import { useState } from "react";
-import axios from "axios";
+import { useEffect, useState } from "react";
+import { AxiosError } from "axios";
+import {
+    addFavorites,
+    deleteFavorites,
+    getFavorites,
+} from "../services/favoritesService";
 
-const useGetFavorites = (name) => {
+export const useGetFavorites = <T>(name: string) => {
     const [isLoading, setLoading] = useState(false);
-        const [error, setError] = useState<AxiosError | null>(null);
-        const [data, setData] = useState<T | null>(null);
-    
-}
+    const [error, setError] = useState<AxiosError | null>(null);
+    const [data, setData] = useState<T | null>(null);
+
+    useEffect(() => {
+        setLoading(true);
+        getFavorites(name)
+            .then((data) => setData(data))
+            .catch((err) => setError(err))
+            .finally(() => setLoading(false));
+    }, [name, data]);
+
+    return { isLoading, error, data };
+};
+
+export const useAddFavorite = () => {
+    const [isLoading, setLoading] = useState(false);
+    const [error, setError] = useState<AxiosError | null>(null);
+    const [data, setData] = useState<string | null>(null);
+
+    const add = (name: string, city: string) => {
+        setLoading(true);
+        addFavorites(name, city)
+            .then((data) => setData(data))
+            .catch((err) => setError(err))
+            .finally(() => setLoading(false));
+    };
+
+    return { add, isLoading, error, data };
+};
+
+export const useDeleteFavorite = () => {
+    const [isLoading, setLoading] = useState(false);
+    const [error, setError] = useState<AxiosError | null>(null);
+    const [data, setData] = useState<string | null>(null);
+
+    const remove = (name: string, city: string) => {
+        setLoading(true);
+        deleteFavorites(name, city)
+            .then((data) => setData(data))
+            .catch((err) => setError(err))
+            .finally(() => setLoading(false));
+    };
+
+    return { remove, isLoading, error, data };
+};
--- client/src/services/favoritesService.ts
+++ client/src/services/favoritesService.ts
@@ -6,16 +6,16 @@ const API = axios.create({
 });
 
 export const getFavorites = async (name: string) => {
-    const res = await API.get(`/${name}`)
-    return res.data
-}
+    const res = await API.get(`/${name}`);
+    return res.data;
+};
 
-export const addFavorites = async () => {
-    const res = await API.post("")
-    return res.data
-}
+export const addFavorites = async (name: string, city: string) => {
+    const res = await API.post("", {}, { params: { name: name, city: city } });
+    return res.data;
+};
 
 export const deleteFavorites = async (name: string, city: string) => {
-    const res = await API.delete("", {params: {name: name, city: city}})
-    return res.data
-}
+    const res = await API.delete("", { params: { name: name, city: city } });
+    return res.data;
+};
